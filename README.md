5192cf091b15be2550d4965b6f2226f23d19ca5a
Trey Moen <50057480+greatgitsby@users.noreply.github.com>
2025-05-12 22:03:36 -0700

    feat: v1 - native clips (#1)
    * add op submodule
    
    * no more replicate
    
    * init basic working version
    
    * use pycord, app command instead of bot listener
    
    * queue, video preview + post
    
    * bump op
    
    * things
    
    * restructure
    
    * bump op
    
    * add README
    
    * build
    
    * no more
    
    * remove op as dep, not needed in current state
    
    * cleanup

---
+++ README.md
@@ -0,0 +1,13 @@
+# openpilot clip app
+
+## setup
+```bash
+git clone https://github.com/greatgitsby/openpilot-clip-discord.git
+cd openpilot-clip-discord
+git submodule update --init --recursive
+cd openpilot
+tools/op.sh setup
+tools/op.sh build
+cd ..
+DISCORD_TOKEN='' uv run main.py
+```
---
+++ main.py
@@ -0,0 +1,111 @@
+import asyncio
+import discord
+import re
+import os
+from tempfile import TemporaryDirectory
+from pathlib import Path
+from urllib.parse import urlparse
+
+link_regex = re.compile(r'https?://\S+')
+route_regex = re.compile(r'\S+/\d+--\S+/\d+/\d+')
+
+queue = asyncio.Queue()
+bot = discord.Bot()
+
+
+class VideoPreview(discord.ui.View):
+  def __init__(self, ctx: discord.ApplicationContext, route: str, vid: discord.File):
+    super().__init__(timeout=None)
+    self.ctx = ctx
+    self.route = route
+    self.vid = vid
+
+  @discord.ui.button(label='Post', style=discord.ButtonStyle.primary, emoji='▶️')
+  async def post_button(self, button: discord.ui.Button, interaction: discord.Interaction):
+    user_id = interaction.user.id
+    await interaction.response.send_message(content=f'<@{user_id}> posted a clip `{self.route}`', file=self.vid)
+    button.label = 'Posted'
+    button.emoji = '✅'
+    button.style = discord.ButtonStyle.green
+    button.disabled = True
+    await self.ctx.edit(view=self)
+
+
+async def process_clip(ctx: discord.ApplicationContext, route: str, title: str):
+  print(f'{ctx.interaction.user.display_name} ({ctx.interaction.user.id}) clipping route `{route}`' )
+  await ctx.edit(content=f'clipping route {route}')
+  try:
+    with TemporaryDirectory() as temp_dir:
+      path = Path(os.path.join(temp_dir, f'{route.replace("/", "-")}.mp4')).resolve()
+      args = ['openpilot/tools/clip/run.py', route, '-o', path, '-f', '10']
+      if title:
+        args.extend(['-t', title])
+      proc = await asyncio.create_subprocess_exec('openpilot/.venv/bin/python3', *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+
+      stdout, stderr = await proc.communicate()
+      if proc.returncode != 0:
+        await ctx.edit(content=f'clip failed due to unknown reason:\n\n```\n{stderr.decode()}\n```')
+      else:
+        await ctx.edit(content='', file=discord.File(path), view=VideoPreview(ctx, route, discord.File(path)))
+  except Exception as e:
+    print('error processing clip', str(e))
+    await ctx.edit(content=f'clip failed due to unknown reason:\n\n```\n{str(e)}\n```')
+
+
+async def worker(name: str):
+  print(f'started worker {name}')
+  while True:
+    ctx, route, title = await queue.get()
+    try:
+      await process_clip(ctx, route, title)
+    finally:
+      queue.task_done()
+
+
+async def preprocess_clip(ctx: discord.ApplicationContext, route: str, title: str):
+  await ctx.defer(ephemeral=True)
+    
+  link = link_regex.match(route)
+  if link:
+      path = urlparse(link.group()).path
+      route = route_regex.match(path[1:])
+      if not route:
+        await ctx.respond(content='no route found in the input provided')
+        return
+  else:
+    route = route_regex.match(route)
+    if not route:
+      await ctx.respond(content='no route found in the input provided')
+      return
+  route = route.group()
+  await ctx.edit(content=f'queued request, {queue.qsize()} in line ahead')
+  await queue.put((ctx, route, title,))
+
+
+@bot.command(
+  description="clip an openpilot route",
+  integration_types={
+    discord.IntegrationType.guild_install,
+    discord.IntegrationType.user_install,
+  },
+)
+@discord.option("route", type=str, description='the route or connect URL with timing info', required=True)
+@discord.option("title", type=str, description='an optional title to overlay', min_length=1, max_length=80, required=False)
+async def clip(ctx: discord.ApplicationContext, route: str, title: str):
+  if ctx.author.bot:
+    return
+  await preprocess_clip(ctx, route, title)
+
+
+@bot.listen(once=True)
+async def on_ready():
+  for i in range(1):
+    await asyncio.create_task(worker(f'clipper-worker-{i}'))
+
+
+if __name__ == "__main__":
+  discord_token = os.environ.get('DISCORD_TOKEN')
+  if discord_token is None:
+    raise EnvironmentError('Missing discord token')
+  bot.run(discord_token)
+
