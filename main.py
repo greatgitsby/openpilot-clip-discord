5c2af747246f6e62dcc9737a92da7f37477485da
Trey Moen <trey@moen.ai>
2025-05-17 16:02:18 -0700

    calls read() internally

--- main.py
+++ main.py
@@ -175,7 +175,7 @@ async def process_clip(request: ClipRequest):
       if proc.returncode != 0:
         await request.post_error(stderr.decode())
       else:
-        file = discord.File(open(path, 'rb'))
+        file = discord.File(path)
         await request.post_success(file)
   except Exception as e:
     print('error processing clip', str(e))
